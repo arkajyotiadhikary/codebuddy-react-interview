4bfc7506c7d7b207c66d8b60e11d734a496b76b4
Arka Jyoti Adhikary <66660068+arkajyotiadhikary@users.noreply.github.com>
2024-06-01 10:27:45 +0530

    Footer alignment fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="bg-gray-800 p-4">
       <div className="container mx-auto text-white">
-        <div className="flex items-center justify-between">
+        <div className="flex flex-col items-center justify-between sm:flex-row">
           <div className="flex items-center">
             <p className="mr-4 text-lg font-bold">CodeBuddy</p>
             <div className="flex space-x-4">
@@ -14,8 +14,8 @@ const Footer = () => {
               </a>
             </div>
           </div>
-          <div className="text-end text-sm text-gray-400">
-            &copy; {new Date().getFullYear()}CodeBuddy
+          <div className="mt-2 text-center text-sm text-gray-400 sm:mt-0 sm:text-end">
+            © {new Date().getFullYear()} CodeBuddy
           </div>
         </div>
       </div>
